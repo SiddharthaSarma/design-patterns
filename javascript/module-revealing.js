e86d7d437653220fc4d908aaa31817b6233aa278
Siddhartha <sidduinevitable@gmail.com>
2018-01-16 10:06:37 +0530

    Create module-revealing pattern

---
+++ javascript/module-revealing.js
@@ -0,0 +1,15 @@
+var Module = (function() {
+
+  var _privateMethod = function() {
+    // private stuff
+  };
+
+  var publicMethod = function() {
+    _privateMethod();
+  };
+
+  return {
+    publicMethod: publicMethod
+  };
+
+})();
